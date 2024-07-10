36ab8823ca48319ec832a2c669a6cfccdeae007f
fernando-c-lima <fernandox.caetano@gmail.com>
2024-07-10 20:39:10 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World React</h1>
+      <h1>Hello World React 1</h1>
       <p>Ansioso para dominar o React!</p>
     </div>
   );
